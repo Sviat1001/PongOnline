3a452534ca13bb46a8ef5981511039062ca8695a
Sviat <sviat1001@gmail.com>
2024-12-29 14:27:53 +0200

    Added Area2D for the goals with two collision shapes, added speed increase for the Ball when the Timer is up, and limited Ball speed to ~4800

--- ball.gd
+++ ball.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
 
 
-var speed = 1200
+var speed = 1200.0
+var speedIncrease = 0.0
 
 func _ready():
 	var direction = Vector2(((-1) ** randi()), 0)
 	velocity = direction * speed
-	var speedIncrease = 0
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
-	
 	if collision:
 		
 		if collision.get_collider() is CharacterBody2D:
 			var paddlePosition = collision.get_collider().global_position
-			var direction = paddlePosition.direction_to(global_position)
-			
+			var direction = paddlePosition.direction_to(global_position)			
 			direction.y = direction.y / 1.5
+			
+			speed += speed * speedIncrease
+			if speed > 4800:
+				speedIncrease = 0
+
 			velocity = direction.normalized() * speed
 			$Hit.play()
 			
@@ -26,4 +29,4 @@ func _physics_process(delta):
 			$WallHit.play()
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	speedIncrease = 0.05
