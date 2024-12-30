b50e84c06dd4e0ad0513e74638b706ade2e284cd
Sviat <sviat1001@gmail.com>
2024-12-30 20:33:55 +0200

    Added Pause mode

--- game.gd
+++ game.gd
@@ -3,15 +3,17 @@ extends Node2D
 var AI
 var score = {'Left': 0, 'Right': 0}
 var sides = {0: "Left", 1: "Right"}
+
 var center
+
 var startFlag = false
+
 signal restart(direction)
 
 func _ready():
 	center = get_viewport().get_visible_rect().size / 2
-	print(AI)
 	
-func start() -> void:
+func start():
 	score = {'Left': 0, 'Right': 0}
 	for side in score.keys():
 		update_score(side)
@@ -22,16 +24,26 @@ func start() -> void:
 	$Ball/TimeOver.start(30)
 	$Ball/TimeRestart.start(1)
 	$CanvasLayer/Message.hide()
-	
-func _process(_delta):
-	if Input.is_key_pressed(KEY_SPACE) and !startFlag:
+
+func _input(event):
+	if event.is_action_pressed("space") and !startFlag:
 		start()
 		startFlag = true
-		
-	if Input.is_key_pressed(KEY_ESCAPE):
-		queue_free()  # Deletes the current game scene
-		get_tree().change_scene_to_file("res://menu.tscn")  # Load the menu scene
 
+func _unhandled_input(event):
+	if event.is_action_pressed("esc"):
+		if !get_tree().paused:
+			$CanvasLayer/Message.text = str('The game is paused \nPress "Escape" to exit and "Space" to resume')
+			$CanvasLayer/Message.show()
+			get_tree().paused = !get_tree().paused
+		else:
+			queue_free()
+			get_tree().change_scene_to_file("res://menu.tscn")
+			
+	elif event.is_action_pressed("space") and get_tree().paused:
+		get_tree().paused = !get_tree().paused
+		$CanvasLayer/Message.hide()
+		
 func _on_goals_body_shape_entered(_body_rid, body, _body_shape_index, local_shape_index):
 	var side = sides[local_shape_index]
 	score[side] += 1
@@ -44,8 +56,10 @@ func _on_goals_body_shape_entered(_body_rid, body, _body_shape_index, local_shap
 		
 		$Paddle1.position.y = center.y
 		$Paddle2.position.y = center.y
-		
-		$CanvasLayer/Message.text = str('Player ' + ("1" if side == "Left" else "2") + ' wins \nPress "Escape" to exit and "Space" to restart')
+		if AI == false:
+			$CanvasLayer/Message.text = str('Player ' + ("1" if side == "Left" else "2") + ' wins \nPress "Escape" to exit and "Space" to restart')
+		else:
+			$CanvasLayer/Message.text = str(("Player" if side == "Left" else "CPU") + ' wins \nPress "Escape" to exit and "Space" to restart')
 		$CanvasLayer/Message.show()
 		startFlag = false
 		$Goals/Victory.play()
--- menu.gd
+++ menu.gd
@@ -1,13 +1,21 @@
 extends Control
 
 var game_scene
-func _ready() -> void:	
+
+func _ready() -> void:    
 	var window = get_window()
 	window.mode = Window.MODE_FULLSCREEN
 	window.position = DisplayServer.screen_get_position()
 	window.size = DisplayServer.screen_get_usable_rect().size
 	window.min_size = Vector2i(800,600)
 	game_scene = preload("res://game.tscn")
+	
+func _on_play_ai_button_pressed() -> void:
+	var game = load("res://game.tscn").instantiate()
+	game.AI = true
+	get_tree().root.add_child(game)
+	queue_free()
+
 
 func _on_play_button_pressed() -> void:
 	var game = load("res://game.tscn").instantiate()
@@ -15,11 +23,6 @@ func _on_play_button_pressed() -> void:
 	get_tree().root.add_child(game)
 	queue_free()
 
+
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-
-func _on_play_ai_button_pressed() -> void:
-	var game = load("res://game.tscn").instantiate()
-	game.AI = true
-	get_tree().root.add_child(game)
-	queue_free()
